8fb0bfb52bc11feaf9e67e2c8502551ed80278e5
Ilenia Piro <ilenia.piro@gmail.com>
2020-01-24 18:45:51 +0100

    Commit gallery

---
+++ main.js
@@ -0,0 +1,23 @@
+$('[data-fancybox="gallery"]').fancybox({
+  gutter: 50,
+  keyboard: true,
+  arrows: true,
+  smallBtn: "auto",
+  animationEffect: "zoom",
+  animationEffect: "fade",
+  animationDuration: 1000,
+});
+const $gallery_items = $(".gallery a");
+
+$("#search").on("keyup", function(event) {
+  const $search = $(event.target).val().toUpperCase();
+  for (i = 0 ; i < $gallery_items.length ; i += 1) {
+    const $search_item = $gallery_items.eq(i);
+    if ($search_item.attr("data-caption").toUpperCase().indexOf($search) === -1) {
+      $search_item.fadeOut();
+    }
+    if ($search_item.attr("data-caption").toUpperCase().indexOf($search) !== -1) {
+      $search_item.hide().fadeIn();
+    }
+  }
+});
